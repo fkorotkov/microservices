a82af65f9e4c6cec8a50da4f2d081e3aad99c740
Fedor Korotkov <fedor.korotkov@gmail.com>
2018-09-28 11:51:29 -0400

    Integrate Multiply Service

--- services/calculator/impl/src/main/kotlin/com/fkorotkov/calculator/evaluator/AsyncEvaluator.kt
+++ services/calculator/impl/src/main/kotlin/com/fkorotkov/calculator/evaluator/AsyncEvaluator.kt
@@ -3,13 +3,15 @@ package com.fkorotkov.calculator.evaluator
 import com.fathzer.soft.javaluator.AbstractEvaluator
 import com.fathzer.soft.javaluator.Operator
 import com.fkorotkov.add.AddServiceClient
+import com.fkorotkov.multiply.MultiplyServiceClient
 import com.fkorotkov.subtract.SubtractServiceClient
 import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.async
 
 class AsyncEvaluator(
   val addServiceClient: AddServiceClient,
-  val subtractServiceClient: SubtractServiceClient
+  val subtractServiceClient: SubtractServiceClient,
+  val multiplyServiceClient: MultiplyServiceClient
 ) : AbstractEvaluator<Deferred<Long>>(Operators.defaultParameters) {
   override fun toValue(literal: String, evaluationContext: Any?): Deferred<Long> = async { literal.toLong() }
 
@@ -20,6 +22,7 @@ class AsyncEvaluator(
       when (operator) {
         Operators.PLUS -> addServiceClient.calculate(a, b)
         Operators.MINUS -> subtractServiceClient.calculate(a, b)
+        Operators.MULTIPLY -> multiplyServiceClient.calculate(a, b)
         else -> throw IllegalStateException("Unknown operator ${operator.symbol}")
       }
     }
--- services/calculator/test-client/src/main/kotlin/com/fkorotkov/calculator/TestCalculatorServiceProvider.kt
+++ services/calculator/test-client/src/main/kotlin/com/fkorotkov/calculator/TestCalculatorServiceProvider.kt
@@ -2,6 +2,7 @@ package com.fkorotkov.calculator
 
 import com.fkorotkov.add.AddServiceClient
 import com.fkorotkov.calculator.impl.CalculatorServiceClientImpl
+import com.fkorotkov.multiply.MultiplyServiceClient
 import com.fkorotkov.services.calculator.grpc.CalculatorGrpc
 import com.fkorotkov.subtract.SubtractServiceClient
 import io.grpc.ManagedChannel
@@ -10,12 +11,16 @@ import io.grpc.inprocess.InProcessServerBuilder
 import java.util.concurrent.TimeUnit
 
 
-class TestCalculatorServiceProvider(addServiceClient: AddServiceClient, subtractServiceClient: SubtractServiceClient) {
+class TestCalculatorServiceProvider(
+    addServiceClient: AddServiceClient,
+    subtractServiceClient: SubtractServiceClient,
+    multiplyServiceClient: MultiplyServiceClient
+) {
   companion object {
     private val testServiceName = "calculator"
   }
 
-  private val serviceImpl: CalculatorServiceImpl = CalculatorServiceImpl(addServiceClient, subtractServiceClient)
+  private val serviceImpl: CalculatorServiceImpl = CalculatorServiceImpl(addServiceClient, subtractServiceClient, multiplyServiceClient)
 
   private val inProcessServer = InProcessServerBuilder.forName(testServiceName)
       .addService(serviceImpl)
